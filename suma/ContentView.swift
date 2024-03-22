3d735e1c338f85c2759be18855673045394d30ed
jesus <jesusteactivat23@gmail.com>
2024-03-22 10:29:51 -0700

    Initial Commit

---
+++ suma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  suma
+//
+//  Created by Jesus on 22/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ suma/sumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sumaApp.swift
+//  suma
+//
+//  Created by Jesus on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct sumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
