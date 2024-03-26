a52b8561f4834ee2eb8a4f02ab6225cb220cfd36
jesus <animalillo44@gmail.com>
2024-03-26 09:37:46 -0700

    mi primer commit

--- suma/ContentView.swift
+++ suma/ContentView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var numero1 = ""
+   @State var numero2 = ""
+    @State var resultado = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("numero 1", text: $numero1)
+        }
+        VStack {
+            TextField("numero 1", text: $numero2)
+        }
+        Button("suma"){
+            sumanumeros() }
+        Text("@resultdo: \(resultado)")
+        
     }
+    func sumanumeros() {
+guard let num1 = Int(numero1), let num2 = Int(numero2) else (return)
+            resultado = num1 + num2
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
